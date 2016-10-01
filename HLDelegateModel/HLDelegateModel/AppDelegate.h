9c90a96389ab5e7c8356ef463c0772894eb39a4a
刘宏立 <liuhongli2120@163.com>
2016-10-01 15:10:23 +0800

    项目创建

---
+++ HLDelegateModel/HLDelegateModel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HLDelegateModel
+//
+//  Created by 刘宏立 on 16/10/1.
+//  Copyright © 2016年 lhl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
